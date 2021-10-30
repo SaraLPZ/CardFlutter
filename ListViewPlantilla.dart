ed9b80498d8f5cac655fd6559dea8697f67a2341
SaraLPZ <79020122+SaraLPZ@users.noreply.github.com>
2021-10-30 19:39:15 +0200

    Create ListViewPlantilla.dart

---
+++ ListViewPlantilla.dart
@@ -0,0 +1,45 @@
+body: Column(children: <Widget>[
+          Container(
+            height: 150,
+            color: Colors.pink,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.black,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ]),
